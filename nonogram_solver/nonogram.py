f87bf96c0f709da14dc1e35368d48cdb3eb8652f
Michele Pratusevich <mprat@alum.mit.edu>
2016-07-17 13:32:16 -0400

    pytest.ini + configs

--- nonogram_solver/nonogram.py
+++ nonogram_solver/nonogram.py
@@ -1,3 +1,11 @@
 """
 Defines the nonogram datatype.
 """
+
+
+class Nonogram(object):
+    def __init__(self):
+        pass
+
+    def init_from_string_box(self, string_box):
+        pass
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,33 +1,38 @@
 from nonogram_solver import solver
+from nonogram_solver.nonogram import Nonogram
 
 
-def test_nonogram_simple():
-    braille_box = [
+def test_nonogram_init():
+    string_box = [
         '.ooo.',
         '.....',
         '.....',
         '.....',
         '.....']
 
-    nonogram = solver.Nonogram(braille_box)
+    nonogram = Nonogram()
+    nonogram.init_from_string_box(string_box)
+
+    assert len(nonogram.rows_constraints) == 5
+    assert len(nonogram.cols_constraints) == 5
+    assert nonogram.rows_constraints[0] == [3]
+    assert all(nonogram.rows_constraints[1:] == [])
+    assert nonogram.cols_constraints[0] == []
+    assert nonogram.cols_constraints[1:4] == [1]
+    assert nonogram.cols_constraints[4] == []
+
+
+def test_nonogram_solve():
+    string_box = [
+        'o..o.',
+        '..o.o',
+        '..oo.',
+        '.o..o',
+        'o...o']
+
+    nonogram = Nonogram()
+    nonogram.init_from_string_box(string_box)
     puzzle_constraints = \
         solver.generate_puzzle_constraints(nonogram)
 
     print puzzle_constraints
-
-
-# braille_box = ['.ooo...o', '.o..o..', '..oo....', '......oo', '.o...o..']
-braille_box = [
-    'o..o.',
-    '..o.o',
-    '..oo.',
-    '.o..o',
-    'o...o']
-
-nonogram = solver.Nonogram(braille_box)
-print nonogram.rows_constraints
-print nonogram.cols_constraints
-puzzle_constraints = \
-    solver.generate_puzzle_constraints(nonogram)
-
-print puzzle_constraints
