20d1d5baf866e1ed5e93f0b9ee557d93953f1f3b
Lee Burton <lburton@lburton.org>
2019-05-04 03:44:49 -0700

    Drop 3.7 which is not on the 14.04 travis worker

--- setup.py
+++ setup.py
@@ -27,8 +27,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.5'
     ],
     keywords='puzzles nonogram solver puzzle',
 
